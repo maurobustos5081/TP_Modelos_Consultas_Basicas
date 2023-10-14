9aff9140c8c64197be06192ef3844157bc412cbd
maurobustos5081 <mauro_18_b@hotmail.com>
2023-10-14 10:28:29 -0300

    Se crea TP Modelos y consultas basicas

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,59 @@
+const db = require('../database/models')
+
+module.exports = {
+    list : (req,res) => {
+
+        db.Movie.findAll()
+            .then((movies)=> {
+                return res.render('moviesList', {
+                    movies 
+                })
+            })
+            .catch((error)=> console.log(error))
+        
+    },
+    detail : (req,res) => {
+        const {id} = req.params;
+        db.Movie.findByPk(id)
+            .then(movie => {
+                /* return res.send(movie) */
+                return res.render('moviesDetail', {
+                    movie
+                })
+            })
+            .catch(error => console.log(error))
+    },
+    new : (req,res) => {
+
+        db.Movie.findAll({
+            order : [
+                ['release_date', 'DESC']
+            ]
+        })
+        .then(movies => {
+            return res.render('newestMovies', {
+                movies
+            })
+        })
+        .catch(error => console.log(error))
+
+        
+    },
+    recomended : (req,res) => {
+
+        db.Movie.findAll({
+            limit : 5,
+            order : [
+                ['rating', 'DESC']
+            ]
+        })
+        .then(movies => {
+            return res.render('recommendedMovies', {
+                movies
+            })
+        })
+        .catch(error => console.log(error))
+
+    }
+    
+}
